03c60e900774cd397160cc8c87afb664d98ad953
Bartosz <radzynskib@gmail.com>
2017-03-14 00:11:46 +0100

    fix?

--- lista/runner.sh
+++ lista/runner.sh
@@ -4,7 +4,7 @@ echo "Running benchmarker"
 echo "tester" > results.txt
 #echo "" > wnioski.txt
 export LD_LIBRARY_PATH=.
-cmake . -DALL=ON -DOPTIMIZATION= >> /dev/null
+cmake . -DALL=ON >> /dev/null
 make >> /dev/null
 echo "----------------------------------testing_with_optimization_lvl-o-----------------------------------------" >>results.txt
 echo "----------------------------------static-----------------------------------------" >>results.txt
